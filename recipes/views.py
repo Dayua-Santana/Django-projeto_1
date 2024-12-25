412ac5784ed5b1edf852030c16b6418e59df9793
Dayuã Santana <daniponsantana@gmail.com>
2024-12-24 21:48:15 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,15 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 #HTTP Request
 def home(request):
-    return render(request,'global/home.html', context={
+    return render(request,'recipes/home.html', context={
         'name':'Dayuã Santana',
     })
-    #Return HTTP Response
-    
-def contato(request):
-    return render(request,'me-apague/temp.html')
-    #Return HTTP Response
-
-def sobre(request):
-    return HttpResponse('Sobre')
     #Return HTTP Response
