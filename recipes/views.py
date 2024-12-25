4e0dca256e7c965dbb797a1bbbf420ba96040a25
Dayuã Santana <daniponsantana@gmail.com>
2024-12-25 14:53:00 -0300

    Ajustando CSS header

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 # Create your views here.
 #HTTP Request
 def home(request):
-    return render(request,'recipes/home.html', context={
+    return render(request,'recipes/pages/home.html', context={
         'name':'Dayuã Santana',
     })
     #Return HTTP Response
